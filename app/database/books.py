5812b8348bab389427d13621f46fdc62336dea56
Thinh Nguyen <thinhnd.ict@gmail.com>
2023-09-18 03:24:05 +0700

    Add function comments

--- app/database/books.py
+++ app/database/books.py
@@ -5,6 +5,11 @@ from app.database import table
 
 
 def create_book(book_data: dict) -> dict:
+    """Creates a new book and auto-generates ID.
+
+    :param book_data: (dict) A dictionary of book info
+    :return: (dict) Created book data
+    """
     # Create a unique ID for the new book
     if book_data.get("id") is None:
         book_id: str = config.BOOK_ID_FORMAT.format(id=uuid.uuid4())
@@ -17,6 +22,11 @@ def create_book(book_data: dict) -> dict:
 
 
 def get_book(book_id: str) -> dict:
+    """Gets a book from the database using its ID.
+
+    :param book_id: (str) ID of the book
+    :return: (dict) Retrieved book data
+    """
     response = table.get_item(Key={"id": book_id})
     book = response.get("Item")
 
